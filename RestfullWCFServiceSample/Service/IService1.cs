82b095b3ab60ad6bee3b153361931330c310279d
mygithubnelsonr@Ug0SgVRPI3sf5SzK4Nwg <mygithub@sys-service.de>
2019-03-22 10:32:12 +0100

    first commit

---
+++ RestfullWCFServiceSample/Service/IService1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace RestfulWCFWebApplication01.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DoWork")]
+        string DoWork();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DoSquare")]
+        int DoSquare(int value);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DoAddValues")]
+        int DoAddValues(AddValues addValues);
+    }
+}
